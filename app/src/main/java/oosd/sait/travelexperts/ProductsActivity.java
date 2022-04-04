9050368843e05655a876e1d5184f4dbd918a06a7
Nate Penner <natepenner@gmail.com>
2022-04-03 23:51:07 -0600

    Added some activities for different sections

---
+++ app/src/main/java/oosd/sait/travelexperts/ProductsActivity.java
@@ -0,0 +1,14 @@
+package oosd.sait.travelexperts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProductsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_products);
+    }
+}
