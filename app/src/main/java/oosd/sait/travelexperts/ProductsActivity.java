de63248fa5c95bbe58aeb74d9ff832ae77adaa34
Nate Penner <natepenner@gmail.com>
2022-04-04 22:28:06 -0600

    Add product functionality now working

--- app/src/main/java/oosd/sait/travelexperts/ProductsActivity.java
+++ app/src/main/java/oosd/sait/travelexperts/ProductsActivity.java
@@ -2,10 +2,14 @@ package oosd.sait.travelexperts;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import oosd.sait.travelexperts.data.DataSource;
@@ -13,28 +17,55 @@ import oosd.sait.travelexperts.data.Product;
 import oosd.sait.travelexperts.data.ProductManager;
 
 public class ProductsActivity extends AppCompatActivity {
-    Button btnFindProduct;
-    EditText etProductId;
-    TextView tvProductName;
+    ListView lvProducts;
+    Button btnAddProduct;
+    ArrayAdapter<Product> adapter;
+    DataSource<Product, Integer> ds;
+
+    public static ProductsActivity instance;
+
+    public static ProductsActivity getInstance() {
+        return instance;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_products);
 
-        DataSource<Product, Integer> ds = new ProductManager(getApplicationContext());
+        instance = this;
+        ds = new ProductManager(getApplicationContext());
 
-        btnFindProduct = findViewById(R.id.btnFindProduct);
-        etProductId = findViewById(R.id.etProductId);
-        tvProductName = findViewById(R.id.tvProductName);
+        lvProducts = findViewById(R.id.lvProducts);
+        btnAddProduct = findViewById(R.id.btnAddProduct);
 
-        btnFindProduct.setOnClickListener(new View.OnClickListener() {
+        adapter = new ArrayAdapter<Product>(this, android.R.layout.simple_list_item_1);
+        lvProducts.setAdapter(adapter);
+
+        loadProducts();
+
+        btnAddProduct.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                int productId = Integer.parseInt(etProductId.getText().toString());
-                Product product = ds.getById(productId);
-                tvProductName.setText(product.getProductName());
+                Intent intent = new Intent(getApplicationContext(), ProductDetailActivity.class);
+                intent.putExtra("mode", "create");
+                startActivity(intent);
             }
         });
+
+        lvProducts.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent(getApplicationContext(), ProductDetailActivity.class);
+                intent.putExtra("mode", "update");
+                intent.putExtra("productId", adapter.getItem(i).getProductId());
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void loadProducts() {
+        adapter.clear();
+        ds.getList().forEach(p -> adapter.add(p));
     }
 }
